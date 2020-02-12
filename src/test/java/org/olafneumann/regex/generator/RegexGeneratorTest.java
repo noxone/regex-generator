a3505481001ba6f78860daa9d4514434d8284ed3
Olaf Neumann <git-account@olafneumann.org>
2020-02-12 08:28:49 +0100

    add more regex

--- src/test/java/org/olafneumann/regex/generator/RegexGeneratorTest.java
+++ src/test/java/org/olafneumann/regex/generator/RegexGeneratorTest.java
@@ -7,17 +7,16 @@ import java.util.Collection;
 import org.junit.jupiter.api.Test;
 
 public class RegexGeneratorTest {
-
 	@Test
 	public void testRecognizeString() throws Exception {
-		final String input = "2020-03-12T13:34:56.123 WARN  [org.olafneumann.test.Test]: This is a simple line";
+		final String input
+				= "2020-03-12T13:34:56.123 WARN  [org.olafneumann.test.Test]: This is #simple. A line with a 'string' in the text";
 		final RegexGenerator generator = new RegexGenerator(Configuration.get());
 
 		final Collection<?> actual = generator.recognize(input);
 
 		actual.forEach(System.out::println);
 
-		assertThat(actual).size().isEqualTo(14);
+		assertThat(actual).size().isEqualTo(19);
 	}
-
 }
