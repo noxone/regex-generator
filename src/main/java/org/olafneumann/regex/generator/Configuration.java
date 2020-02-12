bd5ce25bbc3c63a19f1523fbe6855f115adb5769
Olaf Neumann <git-account@olafneumann.org>
2020-02-12 08:34:10 +0100

    switch from json to yaml configuration

--- src/main/java/org/olafneumann/regex/generator/Configuration.java
+++ src/main/java/org/olafneumann/regex/generator/Configuration.java
@@ -13,17 +13,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 
 public class Configuration {
-	private static final String SETTINGS_FILE_NAME = "settings.json";
+	private static final String SETTINGS_FILE_NAME = "settings.yaml";
 
 	private static final Configuration INSTANCE = readConfiguration();
 
 	private static ObjectMapper createObjectMapper() {
-		final ObjectMapper objectMapper = new ObjectMapper();
+		final ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
 		objectMapper.registerModule(new Jdk8Module());
 		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 		objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
