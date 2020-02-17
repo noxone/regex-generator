c1010e2f5062e6fac7c31b652045a5135f12ffe7
Olaf Neumann <git-account@olafneumann.org>
2020-02-17 22:57:29 +0100

    change regex handling

--- regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/Configuration.kt
+++ regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/Configuration.kt
@@ -5,6 +5,9 @@ data class Configuration(
     val recognizers: List<Recognizer>
 ) {
     companion object {
-        val config = js("require('settings.json')") as Configuration
+        // val config = js("require('settings.json')") as Configuration
+        val config = Configuration(recognizers = listOf(
+                Recognizer("number", "([0-9]+)")
+        ))
     }
 }
