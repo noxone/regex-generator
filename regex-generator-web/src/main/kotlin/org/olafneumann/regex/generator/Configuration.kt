f7341601cf3e6dad34c52bd00a8d1e15fd9dc7ea
Olaf Neumann <git-account@olafneumann.org>
2020-02-18 23:38:04 +0100

    add display ordering

--- regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/Configuration.kt
+++ regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/Configuration.kt
@@ -8,7 +8,8 @@ data class Configuration(
     companion object {
         // val config = js("require('settings.json')") as Configuration
         val default = Configuration(recognizers = listOf(
-                Recognizer("number", "[0-9]+")
+                Recognizer("number", "[0-9]+"),
+            Recognizer("date", "[0-9]{4}-[0-9]{2}-[0-9]{2}")
         ))
     }
 }
--- regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/RegexGenerator.kt
+++ regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/RegexGenerator.kt
@@ -1,22 +1,44 @@
 package org.olafneumann.regex.generator
 
 class RegexGenerator {
-    fun recognize(input: String, config: Configuration = Configuration.default): List<RecognizerMatch> =
+    fun recognize(input: String, config: Configuration = Configuration.default) =
         config.recognizers
             .filter { it.active }
             .flatMap { it.searchRegex.findAll(input).map { mr -> RecognizerMatch(mr.range, extractMainGroup(mr, config), it) }.toList() }
-            .sortedWith(compareBy( { it.range.first }, { it.range.last - it.range.first }, { it.recognizer.name } ))
+            .sortedWith(compareBy( { it.range.first }, { 0 - (it.range.last - it.range.first) }, { it.recognizer.name } ))
 
-    private fun extractMainGroup(matchResult: MatchResult, config: Configuration): String =
+    private fun extractMainGroup(matchResult: MatchResult, config: Configuration) =
         when {
             config.mainGroupName != null -> (matchResult.groups as MatchNamedGroupCollection)[config.mainGroupName]?.value ?: throw Exception("Unable to find group '${config.mainGroupName}'")
             config.mainGroupIndex != null -> matchResult.groups[config.mainGroupIndex]?.value ?: throw Exception("Unable to find group with index ${config.mainGroupIndex}.")
             else -> matchResult.value
         }
+
+    fun organize(findings: List<RecognizerMatch>): Map<RecognizerMatch, Int> {
+        val lines = mutableListOf<Int>()
+        fun getNextFreeLineAt(pos: Int) : Int {
+            var index = lines.indexOfFirst { it < pos }
+            if (index == -1) {
+                index = lines.size
+                lines.add(-1)
+            }
+            return index
+        }
+        fun enter(line: Int, end: Int): Int {
+            lines[line] = end
+            return line
+        }
+        return findings.map { it to enter(getNextFreeLineAt(it.range.first), it.range.last) }.toMap()
+    }
 }
 
 fun main() {
-    RegexGenerator().recognize("2020-12-24 INFO abg.def.h: Dies ist nur ein #beispiel! Und 3!")
-        .forEach { console.info(it) }
+    val generator = RegexGenerator()
+    val findings = generator.recognize("2020-12-24 INFO abg.def.h: Dies ist nur ein #beispiel! Und 3!")
+    val map = generator.organize(findings)
+
+    findings.forEach { console.info(it) }
+    map.forEach { console.info( "${it.key} -> ${it.value}") }
 }
+
 
