a62aae8a60e86726ce2eb9ed6ce8cb6c28ba3d74
Olaf Neumann <git-account@olafneumann.org>
2020-03-04 22:13:28 +0100

    improve RecognizerMatch declaration

--- regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/regex/RecognizerMatch.kt
+++ regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/regex/RecognizerMatch.kt
@@ -1,29 +1,43 @@
 package org.olafneumann.regex.generator.regex
 
-data class RecognizerMatch(
-    val ranges: List<IntRange>,
+class RecognizerMatch(
+    ranges: List<IntRange>,
     val inputPart: String,
     val recognizer: Recognizer
 ) {
-    companion object {
-        val comparator = compareBy<RecognizerMatch>({ it.first }, { -it.length })
-    }
+    val ranges: List<IntRange>
+    val first: Int
+    private val last: Int
+    val length: Int
+
     init {
         if (ranges.isEmpty()) {
             throw RuntimeException("RecognizerMatch without ranges is not allowed.")
         }
+        this.ranges = ranges.sortedWith(compareBy({ it.first }, { it.last }))
+        this.first = this.ranges[0].first
+        this.last = this.ranges[this.ranges.size - 1].last
+        this.length = last - first + 1
     }
-
-    val first: Int = ranges.map { it.first }.min()!!
-    private val last: Int = ranges.map { it.last }.max()!!
-    val length: Int = last - first + 1
 
     fun intersect(other: RecognizerMatch): Boolean =
         ranges.flatMap { thisRange -> other.ranges.map { otherRange -> thisRange to otherRange } }
             .any { it.first.intersect(it.second).isNotEmpty() }
 
+    fun hasSameRangesAs(other: RecognizerMatch): Boolean {
+        if (ranges.size != other.ranges.size) {
+            return false
+        }
+        return ranges.mapIndexed { index, range -> range == other.ranges[index] }
+            .all { it }
+    }
+
     override fun toString(): String =
         "[$first+$length] (${recognizer.name}: ${recognizer.outputPattern}) $inputPart"
+
+    companion object {
+        val comparator = compareBy<RecognizerMatch>({ it.first }, { -it.length })
+    }
 }
 
 
