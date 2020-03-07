b68be5eaba6659c5063c5aab3ec2b783b5e028ac
Olaf Neumann <git-account@olafneumann.org>
2020-03-07 10:20:14 +0100

    prepare to only persist data on user consent

--- regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/ui/ApplicationSettings.kt
+++ regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/ui/ApplicationSettings.kt
@@ -9,19 +9,40 @@ internal object ApplicationSettings {
     private const val KEY_LAST_VERSION = "user.lastVersion"
     private const val VAL_VERSION = 3
 
-    /* Hiding local storage behind functions so we can disable storage if user does not consent */
-    private fun get(key: String) = localStorage.getItem(key)
-    private fun set(key: String, value: String) = localStorage.setItem(key, value)
+    // map to store settings while the user did not consent
+    private val intermediate = mutableMapOf<String, String>()
+    private fun persistIntermediate() {
+        // a simple set will do, because now set() will actually write to localStorage
+        intermediate.forEach { set(it.key, it.value) }
+        intermediate.clear()
+    }
+
+    // Hiding local storage behind functions so we can disable storage if user does not consent
+    private fun get(key: String) = intermediate[key] ?: localStorage.getItem(key)
+    private fun set(key: String, value: String) {
+        if (hasConsent) {
+            localStorage.setItem(key, value)
+        } else {
+            intermediate[key] = value
+        }
+    }
+
+    // conveniance
     private fun set(key: String, value: Int) = set(key, value.toString())
     private fun set(key: String, value: Boolean) = set(key, value.toString())
 
-    /* actual function to access settings */
+    // ----------------------------------
+    // actual function to access settings
+
     fun isNewUser() = get(KEY_LAST_VERSION)?.toIntOrNull() ?: 0 < VAL_VERSION
     fun storeUserLastInfo() = set(KEY_LAST_VERSION, VAL_VERSION)
 
     var hasConsent: Boolean
-        get() = get(KEY_CONSENT)?.toBoolean() ?: false
-        set(value) = set(KEY_CONSENT, value)
+        get() = get(KEY_CONSENT)?.toBoolean() ?: true // TODO switch to false and actually ask!!
+        set(value) {
+            set(KEY_CONSENT, value)
+            persistIntermediate()
+        }
 
     var viewOptions: RecognizerCombiner.Options
         get() = get(KEY_COMBINER_OPTIONS)?.let { JSON.parse<RecognizerCombiner.Options>(it) }
