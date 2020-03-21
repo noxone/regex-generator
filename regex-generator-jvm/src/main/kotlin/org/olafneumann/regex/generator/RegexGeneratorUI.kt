f44a14a4bf0d4248ed43e02af39a0e1e6fbc4e00
Olaf Neumann <git-account@olafneumann.org>
2020-03-22 00:25:49 +0100

    Create real html loading

--- regex-generator-jvm/src/main/kotlin/org/olafneumann/regex/generator/RegexGeneratorUI.kt
+++ regex-generator-jvm/src/main/kotlin/org/olafneumann/regex/generator/RegexGeneratorUI.kt
@@ -1,9 +1,9 @@
 package org.olafneumann.regex.generator
 
-import org.olafneumann.regex.generator.views.javafx.HTML5View
 import javafx.application.Application
+import org.olafneumann.regex.generator.views.javafx.HTML5View
 import tornadofx.App
 
 class HTML5App : App(HTML5View::class)
 
-fun main(args: Array<String>) = Application.launch(HTML5App::class.java)
+fun main(args: Array<String>) = Application.launch(HTML5App::class.java, *args)
