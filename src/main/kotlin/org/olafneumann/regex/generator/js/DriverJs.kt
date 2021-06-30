a5c1deb874317ca34357209f221ea1a767e42f3b
Olaf Neumann <git-account@olafneumann.org>
2021-06-30 09:58:31 +0200

    Suppress warning for too many functions

--- src/main/kotlin/org/olafneumann/regex/generator/js/DriverJs.kt
+++ src/main/kotlin/org/olafneumann/regex/generator/js/DriverJs.kt
@@ -14,7 +14,7 @@ data class Popover(
 fun Driver.defineSteps(stepDefinitions: List<StepDefinition>) =
     defineSteps(stepDefinitions.map { it.asDynamic() }.toTypedArray())
 
-@Suppress("UnusedPrivateMember")
+@Suppress("UnusedPrivateMember", "TooManyFunctions")
 @JsModule("driver.js")
 external class Driver(options: dynamic) {
     /** Checks if the driver is active or not */
