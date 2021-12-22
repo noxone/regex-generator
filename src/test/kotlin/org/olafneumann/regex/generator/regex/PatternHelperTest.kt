3bc59a04199383cf1ec3fdcaff753ba47f06f7bf
Olaf Neumann <git-account@olafneumann.org>
2021-12-22 12:28:41 +0100

    Add test for regex escaping

--- src/test/kotlin/org/olafneumann/regex/generator/regex/PatternHelperTest.kt
+++ src/test/kotlin/org/olafneumann/regex/generator/regex/PatternHelperTest.kt
@@ -4,13 +4,12 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class PatternHelperTest {
-    companion object {
-        private const val given = "abc.\\\$hier \"und\" / 'da'([)."
-    }
-
     @Test
     fun testEscapeForRegex() {
-        val expected = "abc\\.\\\\\\\$hier \"und\" / 'da'\\)\\[\\)\\."
+        // original: abc.\$hier "und" / 'da'([).
+        val given = """abc.\${'$'}hier "und" / 'da'([)."""
+        // expected: abc\.\\\$hier "und" / 'da'\(\[\)\.
+        val expected = """abc\.\\\${'$'}hier "und" / 'da'\(\[\)\."""
 
         val actual = given.escapeForRegex()
 
