9c49a20d5d012b36c45158933d91b352e06162e1
Olaf Neumann <git@olafneumann.org>
2022-01-16 11:07:58 +0100

    Add C language #191

--- src/main/kotlin/org/olafneumann/regex/generator/output/CodeGenerator.kt
+++ src/main/kotlin/org/olafneumann/regex/generator/output/CodeGenerator.kt
@@ -7,7 +7,8 @@ import org.olafneumann.regex.generator.regex.RegexCache
 interface CodeGenerator {
     companion object {
         val all = listOf<CodeGenerator>(
-            CSharpCodeGenerator()
+            CCodeGenerator()
+            , CSharpCodeGenerator()
             , GrepCodeGenerator()
             , JavaCodeGenerator()
             , JavaScriptCodeGenerator()
@@ -273,6 +274,64 @@ public class Sample
             separator = " | ",
             prefix = ", "
         )
+}
+
+internal class CCodeGenerator : SimpleReplacingCodeGenerator(
+    languageName = "C",
+    highlightLanguage = "c",
+    templateCode = """#include <regex.h>
+
+int useRegex(char* textToCheck) {
+    regex_t compiledRegex;
+    int reti;
+    int actualReturnValue = -1;
+    char messageBuffer[100];
+
+    /* Compile regular expression */
+    reti = regcomp(&compiledRegex, "%1${'$'}s", REG_EXTENDED%2${'$'}s);
+    if (reti) {
+        fprintf(stderr, "Could not compile regex\n");
+        return -2;
+    }
+
+    /* Execute compiled regular expression */
+    reti = regexec(&compiledRegex, textToCheck, 0, NULL, 0);
+    if (!reti) {
+        puts("Match");
+        actualReturnValue = 0;
+    } else if (reti == REG_NOMATCH) {
+        puts("No match");
+        actualReturnValue = 1;
+    } else {
+        regerror(reti, &compiledRegex, messageBuffer, sizeof(messageBuffer));
+        fprintf(stderr, "Regex match failed: %s\n", messageBuffer);
+        actualReturnValue = -3;
+    }
+
+    /* Free memory allocated to the pattern buffer by regcomp() */
+    regfree(&compiledRegex);
+    return actualReturnValue;
+}"""
+) {
+
+
+    override fun transformPattern(pattern: String, options: RecognizerCombiner.Options): String =
+        pattern.replace(RegexCache.get("([\\\\\"])"), "\\\\$1").replace(RegexCache.get("\t"), "\\t")
+
+    override fun generateOptionsCode(options: RecognizerCombiner.Options) =
+        options.combine(
+            valueForCaseInsensitive = "REG_ICASE",
+            valueForMultiline = "REG_NEWLINE",
+            separator = " | ",
+            prefix = " | "
+        )
+
+    override fun getWarnings(pattern: String, options: RecognizerCombiner.Options): List<String> {
+        if (options.dotMatchesLineBreaks)
+            return listOf("DOT_ALL is not supported by regex.h")
+
+        return emptyList()
+    }
 }
 
 internal class RubyCodeGenerator : SimpleReplacingCodeGenerator(
--- src/test/kotlin/org/olafneumann/regex/generator/output/CodeGeneratorTest.kt
+++ src/test/kotlin/org/olafneumann/regex/generator/output/CodeGeneratorTest.kt
@@ -68,6 +68,43 @@ public class Sample {
     )
 
     @Test
+    fun testGenerator_C() = testLanguageGenerator(
+        codeGenerator = CCodeGenerator(), expected = """#include <regex.h>
+
+int useRegex(char* textToCheck) {
+    regex_t compiledRegex;
+    int reti;
+    int actualReturnValue = -1;
+    char messageBuffer[100];
+
+    /* Compile regular expression */
+    reti = regcomp(&compiledRegex, "abc\\.\\\\\\${'$'}hier \"und\" / 'da'\\(\\[\\)\\.", REG_EXTENDED | REG_ICASE);
+    if (reti) {
+        fprintf(stderr, "Could not compile regex\n");
+        return -2;
+    }
+
+    /* Execute compiled regular expression */
+    reti = regexec(&compiledRegex, textToCheck, 0, NULL, 0);
+    if (!reti) {
+        puts("Match");
+        actualReturnValue = 0;
+    } else if (reti == REG_NOMATCH) {
+        puts("No match");
+        actualReturnValue = 1;
+    } else {
+        regerror(reti, &compiledRegex, messageBuffer, sizeof(messageBuffer));
+        fprintf(stderr, "Regex match failed: %s\n", messageBuffer);
+        actualReturnValue = -3;
+    }
+
+    /* Free memory allocated to the pattern buffer by regcomp() */
+    regfree(&compiledRegex);
+    return actualReturnValue;
+}"""
+    )
+
+    @Test
     fun testGenerator_CSharp() = testLanguageGenerator(
         codeGenerator = CSharpCodeGenerator(), expected = """using System;
 using System.Text.RegularExpressions;
