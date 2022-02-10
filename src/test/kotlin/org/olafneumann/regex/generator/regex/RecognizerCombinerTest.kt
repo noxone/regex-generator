973d3a6686ccbbc8f9a2b7f7da2a1ea1124ff413
Olaf Neumann <git-account@olafneumann.org>
2022-02-10 10:44:21 +0100

    Add tests for pattern generation #198

---
+++ src/test/kotlin/org/olafneumann/regex/generator/regex/RecognizerCombinerTest.kt
@@ -0,0 +1,53 @@
+package org.olafneumann.regex.generator.regex
+
+import kotlin.math.exp
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class RecognizerCombinerTest {
+    companion object {
+        private val recognizer = EchoRecognizer("dummy", "dummy")
+        private const val inputText = "abc123abc"
+        private val selectedMatches = listOf(
+            RecognizerMatch(
+                patterns = listOf("[a-z]+"),
+                ranges = listOf(IntRange(0, 2)),
+                recognizer = recognizer,
+                title = "dummy"
+            ),
+            RecognizerMatch(
+                patterns = listOf("[a-z]+"),
+                ranges = listOf(IntRange(6, 8)),
+                recognizer = recognizer,
+                title = "dummy"
+            )
+        )
+
+    }
+
+    @Test
+    fun testMatchCombinationWithoutOnlyPatterns() {
+        val expected = "[a-z]+123[a-z]+"
+
+        val actual = RecognizerCombiner.combineMatches(
+            inputText = inputText,
+            selectedMatches = selectedMatches,
+            options = RecognizerCombiner.Options(onlyPatterns = false)
+        ).pattern
+
+        assertEquals(expected = expected, actual = actual)
+    }
+
+    @Test
+    fun testMatchCombinationWithOnlyPatterns() {
+        val expected = "[a-z]+.*[a-z]+"
+
+        val actual = RecognizerCombiner.combineMatches(
+            inputText = inputText,
+            selectedMatches = selectedMatches,
+            options = RecognizerCombiner.Options(onlyPatterns = true)
+        ).pattern
+
+        assertEquals(expected = expected, actual = actual)
+    }
+}
