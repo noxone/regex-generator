8b4bf6b8b17be8a3a6cc7a19dfd7c2538f591ea3
Olaf Neumann <git-account@olafneumann.org>
2022-03-16 07:45:17 +0100

    Add test to ensure escaped pipes #201

---
+++ src/test/kotlin/RegexGenerationTest.kt
@@ -0,0 +1,31 @@
+import org.olafneumann.regex.generator.regex.RecognizerCombiner
+import org.olafneumann.regex.generator.regex.RecognizerCombiner.Options
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class RegexGenerationTest {
+    companion object {
+        private fun assertRegexIsCorrect(given: String, expected: String, options: Options = Options()) {
+            val actual =
+                RecognizerCombiner.combineMatches(inputText = given, selectedMatches = emptyList(), options = options)
+
+            assertEquals(expected = expected, actual = actual.pattern)
+        }
+    }
+
+    @Test
+    fun testRegex01() {
+        val given = "abc["
+        val expected = "abc\\["
+
+        assertRegexIsCorrect(given = given, expected = expected)
+    }
+
+    @Test
+    fun testRegex02() {
+        val given = "| | | 13024a.htm"
+        val expected = "\\| \\| \\| 13024a\\.htm"
+
+        assertRegexIsCorrect(given = given, expected = expected)
+    }
+}
