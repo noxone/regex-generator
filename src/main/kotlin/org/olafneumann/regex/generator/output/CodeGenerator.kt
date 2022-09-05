261ef62c8032408ed9b0f5c11a6118a3989be880
Olaf Neumann <github@olafneumann.org>
2022-09-05 07:08:25 +0200

    #259 Write down code more clearly and document it better

--- src/main/kotlin/org/olafneumann/regex/generator/output/CodeGenerator.kt
+++ src/main/kotlin/org/olafneumann/regex/generator/output/CodeGenerator.kt
@@ -398,20 +398,21 @@ internal class PythonCodeGenerator : SimpleReplacingCodeGenerator(
     templateCode = """import re
 
 def use_regex(input_text):
-    pattern = re.compile(r"%1${'$'}s%2${'$'}s)
+    pattern = re.compile(%1${'$'}s%2${'$'}s)
     return pattern.match(input_text)"""
 ) {
     override fun transformPattern(pattern: String, options: RecognizerCombiner.Options): String {
-        return (
-                pattern
-                    // escape quotation mark through backslashes
-                    .replace(RegexCache.get("(\")"), "\\\\\"")
-                    // handle backslash at end of string (but this might add an empty r-string)
-                    .replace(RegexCache.get("""(\\+)$"""), "\"'$1$1'r\"")
-                // add trailing quotation mark because it is not available in the template!
-                + '"'
-            )
-            // remove unnecessary empty r-string at end of sequence
+        val escapedPattern = pattern
+            // escape quotation mark through backslashes
+            .replace(RegexCache.get("(\")"), "\\\\\"")
+            // handle backslash at end of string (but this might add an empty r-string)
+            .replace(RegexCache.get("""(\\+)$"""), "\"'$1$1'r\"")
+
+        // The template does not contain quotation marks, because they depend on the pattern content.
+        // By default, this generator uses r-string (which have nearly no escape sequences).
+        return "r\"${escapedPattern}\""
+            // But this may lead to empty r-strings in the beginning or end of the string. To generate more pretty code
+            // these empty r-strings will be removed.
             .replace(RegexCache.get("^r\"\"|r\"\"$"), "")
     }
 
