992d927bcbb9ee399d5f94ae7226493f6b0ab0c7
Olaf Neumann <github@olafneumann.org>
2022-11-05 11:14:40 +0100

    add link

--- src/main/kotlin/org/olafneumann/regex/generator/RegexGenerator.kt
+++ src/main/kotlin/org/olafneumann/regex/generator/RegexGenerator.kt
@@ -12,6 +12,8 @@ fun main() {
 
 // TODO add service worker:
 // https://resources.jetbrains.com/storage/products/kotlinconf2019/slides/Thursday/Aud%2012/E.%20Hellman,%20Building%20Progressive%20Web%20Apps%20%20in%20Kotlin.pdf
+// PWA STUFF
+// https://web.dev/app-like-pwas/
 
 /* the caught exception is generic to really catch all exceptions */
 @Suppress("TooGenericExceptionCaught")
