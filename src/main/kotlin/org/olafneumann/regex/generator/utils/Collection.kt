8159be37b6b9b891c343925f5af5d0c68441e2d3
Olaf Neumann <github@olafneumann.org>
2023-02-11 18:29:30 +0100

    add some utility functions

--- src/main/kotlin/org/olafneumann/regex/generator/utils/Collection.kt
+++ src/main/kotlin/org/olafneumann/regex/generator/utils/Collection.kt
@@ -1,7 +1,9 @@
 package org.olafneumann.regex.generator.utils
 
-fun Collection<Any>.toIndexedString(prefix: String = ""): String =
-    this
-        .mapIndexed { index, recognizerMatch -> index to recognizerMatch }
+fun Collection<Any>.toIndexedString(prefix: String = ""): String = asSequence().toIndexedString(prefix = prefix)
+
+fun <T> Sequence<T>.toIndexedString(prefix: String = ""): String =
+    mapIndexed { index, recognizerMatch -> index to recognizerMatch }
         .joinToString(separator = "\n", prefix = prefix) { "${it.first}: ${it.second}" }
 
+fun <T> Sequence<T>.peek(action: (T) -> Unit) = map { action(it); it }
