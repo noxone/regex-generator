3a3b318c2f0ebfa59f54ef8482b90f6ecd73ecdc
Olaf Neumann <github@olafneumann.org>
2025-03-15 19:33:42 +0100

    Sonar: Introduce version library

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ group = "org.olafneumann.regexgenerator"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    kotlin("multiplatform") version "2.1.10"
-    id("io.gitlab.arturbosch.detekt").version("1.23.6")
-    id("org.sonarqube") version "6.0.1.5171"
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.sonarqube)
 }
 
 
@@ -22,18 +22,12 @@ repositories {
 kotlin {
     sourceSets {
         jsMain.dependencies {
-            implementation("org.jetbrains.kotlinx:kotlinx-html:0.12.0")
-
-            implementation("org.jetbrains.kotlin:kotlin-stdlib-js")
-
-            implementation("org.jetbrains:kotlin-extensions:1.0.1-pre.156-kotlin-1.5.0")
-
-            implementation("io.ktor:ktor-client-core:3.1.1")
-
-            implementation("io.ktor:ktor-client-js:3.1.1")
-
-            implementation("io.github.petertrr:kotlin-multiplatform-diff:1.0.0")
-
+            implementation(libs.kotlinx.html)
+            implementation(kotlin("stdlib-js"))
+            implementation(libs.kotlin.extension)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.js)
+            implementation(libs.kotlin.multiplatform.diff)
             implementation(npm("driver.js", "0.9.8"))
         }
         jsTest.dependencies {
